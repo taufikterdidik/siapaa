6a162b2e31c1d5acaf68779ecda4945a192bbb16
Taufik Fahrudin <taufik@terdidik.com>
2021-12-09 21:56:50 +0700

    initial commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,56 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'SIAPAA';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">SIAPAA</h1>
+
+        <p class="lead">Sistem Informasi Pekerti dan Applied Approach</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Mulai</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>PEKERTI</h2>
+
+                <p>rogram Peningkatan Keterampilan Dasar Teknik Instruksional (PEKERTI) merupakan program pelatihan yang dapat 
+                    dimanfaatkan untuk  meningkatkan kompetensi dosen dalam memangku jabatan fungsional, terutama pada 
+                    peningkatan keterampilan pedagogis. Pelatihan PEKERTI ditujukan bagi dosen agar menguasai konsep-konsep 
+                    dasar dalam pembelajaran  dan memiliki kemampuan mengajar yang memadai</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>AA</h2>
+
+                <p>Program Applied Approach (AA) dirancang sebagai program lanjutan dari program PEKERTI. Program ini melatih 
+                    dosen untuk memiliki wawasan dalam pembelajaran yang meliputi materi tentang Kebijakan Pengembangan 
+                    Profesionalisme Dosen dalam Pelaksanaan Mutu Pembelajaran, Etika Moral dalam Pembelajaran, Manajemen Mutu 
+                    Terpadu, Konstruktivisme dalam pembelajaran, Rekonstruksi Mata kuliah, Penulisan Bahan Ajar, Konsep Dasar 
+                    dan Paradigma Pengembangan Kurikulum, Model-Model Pembelajaran Aktif dan Inovatif, Evaluasi PBM, Assessment 
+                    Alternatif, dan Aplikasi Penelitian Tindakan Kelas (PTK).</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>MOOC</h2>
+
+                <p>MOOCs, atau Massive Open Online Courses adalah perkuliahan daring yang menawarkan akses terbuka melalui internet 
+                    secara gratis atau dengan biaya kecil. Peserta MOOC dapat mencapai angka yang sangat tinggi, hingga lebih dari
+                     100,000 pelajar dalam waktu yang bersamaan. MOOC juga terbuka, yang berarti bahwa siapapun, dimanapun dapat 
+                     mendaftarkan diri untuk mendapatkan perkuliahan tersebut. Selain itu, MOOC juga dilaksanakan melalui internet, 
+                     dan seringkali memiliki materi yang mirip dengan mayoritas perkuliahan daring universitas.</p>
+
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
